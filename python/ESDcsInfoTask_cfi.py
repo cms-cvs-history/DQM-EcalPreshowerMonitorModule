70691a91da653e2c34e68032e26c2ebbbca9b96d
cmsbuild <>
2009-11-12 15:14:05 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalPreshowerMonitorModule-V00-02-28'.
    Sprout from master 2009-11-12 15:14:04 UTC Yeong-Jyi Lei <sha1-4336116f62c995f0b2b10f48d64d3d17454deeff@cern.ch> 'add dcsStatus'
    Cherrypick from master 2009-09-14 07:43:40 UTC Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch> 'first commit of the DCS Info task.':
        python/ESDcsInfoTask_cfi.py

--- python/ESDcsInfoTask_cfi.py
+++ python/ESDcsInfoTask_cfi.py
@@ -3,6 +3,5 @@ import FWCore.ParameterSet.Config as cms
 ecalPreshowerDcsInfoTask = cms.EDAnalyzer("ESDcsInfoTask",
     prefixME = cms.untracked.string('EcalPreshower'),
     enableCleanup = cms.untracked.bool(False),
-    mergeRuns = cms.untracked.bool(False),
-    DcsStatusLabel = cms.InputTag("scalersRawToDigi")
+    mergeRuns = cms.untracked.bool(False)
 )
